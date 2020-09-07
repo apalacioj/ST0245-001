f2ea9b709bbfc1eae27a3e34ed2b874ccea19d7e
apalacioj <68928378+apalacioj@users.noreply.github.com>
2020-09-06 23:36:56 -0500

    Cambios en complejidad Array3

--- laboratorios/lab02/ejercicioEnLinea/CodingBat.java
+++ laboratorios/lab02/ejercicioEnLinea/CodingBat.java
@@ -1,5 +1,10 @@
+/**
+*Esta clase contiene los ejercicios en línea para el laboratorio 2
+*@author Alejandra Palacio y Valentina Moreno
+*@version 06/09/2020
+*/
 public class CodingBat{
-
+     //EJERCICIOS ARRAY2
      public int countEvens(int[] nums) {
         int cont = 0;                               // T(n) = c1 
         for(int i = 0; i < nums.length; i++){       // T(n) = c2*n
@@ -49,84 +54,82 @@ public class CodingBat{
         return sum == 8;                           // T(n) = c4
       }
 
-
+    //EJERCICIOS ARRAY3
     public int[] seriesUp(int n) {
-        int [] ans = new int [n*(n+1)/2]; //C1
-        int pos = 0; //C2
-        for(int i = 1; i <= n; i++){ //C3*n
-          for(int j = 1;j <= i; j++){ //C4*n*m Como m <= n, entonces m = n C4*l*l 
-            ans[pos] = j; //C5*n*m //C5*n*n
-            pos++; //C6*n*m, entonces C6*n*n
+        int [] ans = new int [n*(n+1)/2];          // T(n) = c1
+        int pos = 0;                               // T(n) = c2
+        for(int i = 1; i <= n; i++){               // T(n) = c3*n
+          for(int j = 1;j <= i; j++){              // T(n) = c4*n*m, en el peor de los casos n = m, por tanto T(n) = c4*n*n 
+            ans[pos] = j;                          // T(n) = c5*n*m, en el peor de los casos n = m, por tanto T(n) = c5*n*n
+            pos++;                                 // T(n) = c6*n*m, en el peor de los casos n = m, por tanto T(n) = c6*n*n
           }
         }
-        return ans; //C7
-      }
+        return ans;                                // T(n) = c7
+      }                                            // T(n) = c1 + c2 + c3n + c4n*n + c5n*n + c6n*n + c7
 
      public int maxSpan(int[] nums) {
-         if(nums.length == 0){ //c1
-           return 0; //c2
+         if(nums.length == 0){                                   // T(n) = c1
+           return 0;                                             // T(n) = c2
            } 
-         int maxSpan = 1; //c3
-         int span = 0 ; //c4
-         for(int i = 0; i < nums.length; i++){ //c5*n
-               for(int j = nums.length - 1; j >= 0; j--){ //c6*n*n
-                    if(nums[j] == nums[i]){ //c7*n*n
-                    span = j - i + 1; //c8*n*n
-                    if(span > maxSpan){ //c9*n*n
-                    maxSpan = span; //c10*n*n
+         int maxSpan = 1;                                        // T(n) = c3
+         int span = 0 ;                                          // T(n) = c4
+         for(int i = 0; i < nums.length; i++){                   // T(n) = c5*n
+               for(int j = nums.length - 1; j >= 0; j--){        // T(n) = c6*n*n
+                    if(nums[j] == nums[i]){                      // T(n) = c7*n*n
+                    span = j - i + 1;                            
+                    if(span > maxSpan){                          // T(n) = c8*n*n
+                    maxSpan = span;                              
                   }
                 }
            }
       }
-  return maxSpan; //C11
-}
+  return maxSpan;                                                // T(n) = c9
+}                                                                // T(n) = c1 + c2 + c3 + c4 + c5n + c6n*n + c7n*n + c8n*n + c9
 
     public int[] fix34(int[] nums) {
-        int t=0;                                  //c1
+        int t=0;                                                 // T(n) = c1
        
-        for(int i=0; i< nums.length ; i++){       //c2 * n
-           for(int j=0;j<nums.length ; j++){      //c3 * n * n
-               if(nums[i]==4 && nums[j]==3){      //c4 * n * n
-                  t=nums[j+1];                    //c5 * n * n
-                  nums[j+1]=nums[i];              //c6 * n * n
-                  nums[i]=t;                      //c7 * n * n
+        for(int i=0; i< nums.length ; i++){                      // T(n) = c2 * n
+           for(int j=0;j<nums.length ; j++){                     // T(n) = c3 * n * n
+               if(nums[i]==4 && nums[j]==3){                     // T(n) = c4 * n * n
+                  t=nums[j+1];                                   
+                  nums[j+1]=nums[i];                            
+                  nums[i]=t;                                    
                }
            }
         }
-      return nums;                               //c8
-    }
+      return nums;                                               // T(n) = c5
+    }                                                            // T(n) = c1 + c2n + c3n*n + c4n*n + c5
 
     public boolean canBalance(int[] nums) {
-        int acum1 = 0;                                  //C1
-        int acum2 = 0;                                  //C2
-        for(int i = 0; i < nums.length; i++){           //C3*n
-          acum1 += nums[i];                             //C4*n
-            for(int j = i+1; j < nums.length; j++){       //C5*n*(n-1)
-                acum2 += nums[j];                           //C6*n*(n-1)
+        int acum1 = 0;                                           // T(n) = c1
+        int acum2 = 0;                                           // T(n) = c2
+        for(int i = 0; i < nums.length; i++){                    // T(n) = c3*n
+          acum1 += nums[i];                                      // T(n) = c4*n
+            for(int j = i+1; j < nums.length; j++){              // T(n) = c5*n*(n-1)
+                acum2 += nums[j];                                // T(n) = c6*n*(n-1)
             }
-            if(acum1 == acum2){                           //C7*n
-             return true;                                //C8*n
+            if(acum1 == acum2){                                  // T(n) = c7*n
+             return true;                                        
             }   
-         acum2 = 0;                                    //C9*n
+         acum2 = 0;                                              // T(n) = c8*n
         }
-        return false;                                   //C10
-      }
+        return false;                                            // T(n) = c9
+      }                                                          // T(n) = c1 + c2 + c3n + c4n + c5n(n-1) + c6n(n-1) + c7n + c8n + c9
 
     public boolean linearIn(int[] outer, int[] inner) {
-        int count = 0;                                      //c1
-        for (int i = 0; i < inner.length; i++) {            //c2*n
-            for (int j = 0; j < outer.length; j++) {        //c3 * m * n, como m <= n, entonces c3 * n * n
-                if (inner[i] == outer[j]) {                 //c4 * m * n, entonces c4 * n * n
-                     count++;                               //c5 * m * n, entonces c5 * n * n
-                     break;                                 //c6 * m * n, entonces c6 * n * n
+        int count = 0;                                           // T(n) = c1
+        for (int i = 0; i < inner.length; i++) {                 // T(n) = c2*n
+            for (int j = 0; j < outer.length; j++) {             // T(n) = c3 * m * n, en el peor de los casos los arreglos son iguales, es decir m=n, por tanto, T(n) =  c3 * n * n
+                if (inner[i] == outer[j]) {                      // T(n) = c4 * m * n, en el peor de los casos los arreglos son iguales, es decir m=n, por tanto, T(n) =  c4 * n * n
+                     count++;                                   
+                     break;                                 
                 }
             }
         }
-        if (count == (inner.length)){
-             return true;                                   //c7
+        if (count == (inner.length)){                            // T(n) = c5
+             return true;                                   
         }
-        return false;                                       //c8
-    }
-
-      
+        return false;                                            // T(n) = c6
+    }                                                            // T(n) = c1 + c2n + c3n*n + c4n*n + c5+ c6
 }
