b4c73190b71895835a256d9c8f9ca911565d722a
Valentina Moreno Ramírez <68928379+vmorenor16@users.noreply.github.com>
2020-10-11 18:11:00 -0500

    Add files via upload

--- proyecto/codigo/ImpurezaGini.java
+++ proyecto/codigo/ImpurezaGini.java
@@ -1,13 +1,15 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.ArrayList;
 /**
  * Esta clase tendrá un método que será capaz de leer cualquier archivo cvs que el usuario le pase
  * @author Alejandra Palacio y Valentina Moreno 
  * @date 16/08/20
+ * @version 1
  */
 public class ImpurezaGini{
     /**
@@ -16,32 +18,45 @@ public class ImpurezaGini{
     public static final String COMMA_DELIMITER = ";";
 
     /**
-     * Este método main nos permitirá obtener el mejor Gini posible con unas condiciones ya preestablecidas en cada categoría escogida
+     * Este método main nos permitirá leer el archivo preestablecido e imprimirá los elementos de este
      * @param args
      */
     public static void main(String[] args) {
-        List<List<String>> register = readCSV("Datos.csv");
-
-
-        Gini gini1 = new Gini(calculateGiniImpurity(13, register, 2),"Número de libros", "Más de 100 libros");
-        Gini gini2 = new Gini(calculateGiniImpurity(35, register, 1),"El estudiante tiene internet en el hogar?", "Sí");
-        Gini gini3 = new Gini(calculateGiniImpurity(36, register, 1),"El estudiante tiene computador en el hogar?", "Sí");
-        Gini gini4 = new Gini(calculateGiniImpurity(42, register, 1),"El estudiante tiene celular?", "Sí");
-        Gini gini5 = new Gini(calculateGiniImpurity(54, register, 1),"Colegio Bilingüe", "Más de 100 libros");
-        Gini gini6 = new Gini(calculateGiniImpurity(65, register, 66.5),"Puntuación lenguaje", "Puntuación mayor a 50");
-        Gini gini7 = new Gini(calculateGiniImpurity(66, register, 89),"Puntuación matemática", "Puntuación mayor a 50");
-        Gini gini8 = new Gini(calculateGiniImpurity(67, register, 50),"Puntuación biología", "Puntuación mayor a 50");
-
-        System.out.println(gini1.getGini() + "    " + gini1.getVariable());
-        System.out.println(gini2.getGini() + "    " + gini2.getVariable());
-        System.out.println(gini3.getGini() + "    " + gini3.getVariable());
-        System.out.println(gini4.getGini() + "    " + gini4.getVariable());
-        System.out.println(gini5.getGini() + "    " + gini5.getVariable());
-        System.out.println(gini6.getGini() + "    " + gini7.getVariable());
-        System.out.println(gini8.getGini() + "    " + gini8.getVariable());
+        List<List<String>> register = readCSV("lite.csv");
+        String [] cond1 = conditions(register, 68);
+        String [] cond2 = conditions(register, 69);
+        String [] cond3 = conditions(register, 70);
+        String [] cond4 = conditions(register, 71);
+        String [] cond5 = conditions(register, 72);
+        String [] cond6 = conditions(register, 13);
+        String [] cond7 = conditions(register, 35);
+        String [] cond8 = conditions(register, 65);
+        String [] cond9 = conditions(register, 66);
+        String [] cond10 = conditions(register, 67);
+        String [] cond11 = conditions(register, 75);
+        Gini gini1 = getMinGini(register, 68, cond1);
+        Gini gini2 = getMinGini(register, 69, cond2);
+        Gini gini3 = getMinGini(register, 70, cond3);
+        Gini gini4 = getMinGini(register, 71, cond4);
+        Gini gini5 = getMinGini(register, 72, cond5);
+        Gini gini6 = getMinGini(register, 13, cond6);
+        Gini gini7 = getMinGini(register, 35, cond7);
+        Gini gini8 = getMinGini(register, 65, cond8);
+        Gini gini9 = getMinGini(register, 66, cond9);
+        Gini gini10 = getMinGini(register, 67, cond10);
+        Gini gini11 = getMinGini(register, 75, cond11);
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini1.getVariable()+" es "+gini1.getCondition()+". Esta impureza es "+gini1.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini2.getVariable()+" es "+gini2.getCondition()+". Esta impureza es "+gini2.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini3.getVariable()+" es "+gini3.getCondition()+". Esta impureza es "+gini3.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini4.getVariable()+" es "+gini4.getCondition()+". Esta impureza es "+gini4.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini5.getVariable()+" es "+gini5.getCondition()+". Esta impureza es "+gini5.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini6.getVariable()+" es "+gini6.getCondition()+". Esta impureza es "+gini6.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini8.getVariable()+" es "+gini8.getCondition()+". Esta impureza es "+gini8.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini9.getVariable()+" es "+gini9.getCondition()+". Esta impureza es "+gini9.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini10.getVariable()+" es "+gini10.getCondition()+". Esta impureza es "+gini10.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini11.getVariable()+" es "+gini11.getCondition()+". Esta impureza es "+gini11.getGini());
+        System.out.println("La condición que reduce la impureza de gini de la variable "+gini7.getVariable()+" es "+gini7.getCondition()+". Esta impureza es "+gini7.getGini());
     }
-
-
 
     /**
      * Este método le preguntará al usuario el nombre del archivo cvs que desee leer y procederá a guardarlos en un arraylist doble que después retornará 
@@ -73,76 +88,70 @@ public class ImpurezaGini{
      * @param condition, condición para clasificar los valores
      * @return Valor del gini en la columna dada teniendo en cuenta la condición
      */
-    public static double calculateGiniImpurity(int column, List<List<String>> r, double condition){
-
+    public static Gini calculateGiniImpurity(int column, List<List<String>> r, String condition){
         int contLeft = 0; 
         int contRight = 0;
         int contPositiveLeft = 0;
         int contNegativeLeft = 0;
         int contPositiveRight = 0;
         int contNegativeRight = 0;
+        boolean a = true;
 
-        for(int i = 1; i < r.get(i).size(); i++){
-
-            if(r.get(i).get(column).equalsIgnoreCase("Si")){
-                r.get(i).set(r.indexOf(r.get(column)), "1");
-            }
-            if(r.get(i).get(column).equalsIgnoreCase("No")){
-                r.get(i).set(r.indexOf(r.get(column)), "0");
-            }
-
-            if(r.get(i).get(column).equalsIgnoreCase("S")){
-                r.get(i).set(r.indexOf(r.get(column)), "1");
+        for(int i = 1; i < r.size(); i++){
+            try{
+                double n = Double.parseDouble(r.get(i).get(column));
+                a = false;
+            }catch(NumberFormatException e){
+                a = true;
             }
 
-            if(r.get(i).get(column).equalsIgnoreCase("N")){
-                r.get(i).set(r.indexOf(r.get(column)), "0");
-            }
-
-            if(r.get(i).get(column).equalsIgnoreCase("0 A 10 LIBROS")){
-                r.get(i).set(r.indexOf(r.get(column)), "1");
-            }
-
-            if(r.get(i).get(column).equalsIgnoreCase("11 A 25 LIBROS")){
-                r.get(i).set(r.indexOf(r.get(column)), "2");
-            }
-
-            if(r.get(i).get(column).equalsIgnoreCase("26 A 100 LIBROS")){
-                r.get(i).set(r.indexOf(r.get(column)), "3");
-            }
-
-            if(r.get(i).get(column).equalsIgnoreCase("MÁS DE 100 LIBROS")){
-                r.get(i).set(r.indexOf(r.get(column)), "4");
-            }
-            
-            if(r.get(i).get(column).equals("")){
-                continue;
-            }
-
-            else if(Double.parseDouble(r.get(i).get(column)) >= condition){
-                contLeft++;
-                if(r.get(i).get(r.get(i).size()-1).equals("0")){
-                    contNegativeLeft++;
+            if(a == true){
+                if(r.get(i).get(column).equals("")){
+                    continue;
+                }else if(r.get(i).get(column).equals(condition)){
+                    contLeft++;
+                    if(r.get(i).get(r.get(i).size()-1).equals("0")){
+                        contNegativeLeft++;
+                    }
+                    else if(r.get(i).get(r.get(i).size()-1).equals("1")){
+                        contPositiveLeft++;
+                    }
+                }else{
+                    contRight++;
+                    if(r.get(i).get(r.get(i).size()-1).equals("0")){
+                        contNegativeRight++;
+                    }
+                    else if(r.get(i).get(r.get(i).size()-1).equals("1")){
+                        contPositiveRight++;
+                    }
                 }
-                else if(r.get(i).get(r.get(i).size()-1).equals("1")){
-                    contPositiveLeft++;
-                }
-            }
-            else{
-                contRight++;
-                if(r.get(i).get(r.get(i).size()-1).equals("0")){
-                    contNegativeRight++;
-                }
-                else if(r.get(i).get(r.get(i).size()-1).equals("1")){
-                    contPositiveRight++;
+            }else{
+                if(r.get(i).get(column).equals("")){
+                    continue;
+                }else if(Double.parseDouble(r.get(i).get(column)) >= Double.parseDouble(condition)){
+                    contLeft++;
+                    if(r.get(i).get(r.get(i).size()-1).equals("0")){
+                        contNegativeLeft++;
+                    }
+                    else if(r.get(i).get(r.get(i).size()-1).equals("1")){
+                        contPositiveLeft++;
+                    }
+                }else{
+                    contRight++;
+                    if(r.get(i).get(r.get(i).size()-1).equals("0")){
+                        contNegativeRight++;
+                    }
+                    else if(r.get(i).get(r.get(i).size()-1).equals("1")){
+                        contPositiveRight++;
+                    }
                 }
             }
         }
 
-        double leftIndex = 1 - (Math.pow(contPositiveLeft/contLeft, 2)+ Math.pow(contNegativeLeft/contLeft, 2));
-        double rightIndex = 1 - (Math.pow(contPositiveRight/contRight, 2)+ Math.pow(contNegativeRight/contRight, 2));
+        double leftIndex = 1 - (Math.pow(contPositiveLeft/(double)contLeft, 2)+ Math.pow(contNegativeLeft/(double)contLeft, 2));
+        double rightIndex = 1 - (Math.pow(contPositiveRight/(double)contRight, 2)+ Math.pow(contNegativeRight/(double)contRight, 2));
 
-        return balancedGini(leftIndex, rightIndex, contLeft, contRight);
+        return new Gini(balancedGini(leftIndex, rightIndex, contLeft, contRight), String.valueOf(column), String.valueOf(condition));
 
     }
 
@@ -156,10 +165,29 @@ public class ImpurezaGini{
      */
     public static double balancedGini(double leftIndex, double rightIndex, int totalLeft, int totalRight){
         return ((totalLeft*leftIndex) + (totalRight*rightIndex)) / (totalLeft + totalRight);
-
     }
 
-    /**Este método nos permitirá obtener el mejor gini posible 
+    /**
+     * Este método nos permitirá sacar las condiciones en la que se evaluará el Gini para así obtener el mejor valor
+     * @param r archivo con los datos que serán analizados
+     * @param column columna a la que se le sacarán las condiciones
+     * @return array de condiciones
+     */
+    public static String[] conditions(List<List<String>> r, int column){
+        ArrayList<String> conditionsTemp = new ArrayList<>();
+        int j = 0;
+        for(int i = 1; i<r.size(); i++){
+            if(r.get(i).get(column) == ""){
+                continue;
+            }else{
+                conditionsTemp.add(r.get(i).get(column));
+            }
+        }
+        String [] conditions = new String[conditionsTemp.size()];
+        return conditionsTemp.toArray(conditions);
+    }
+
+   /**Este método nos permitirá obtener el mejor gini posible 
      * @param r, lista de datos a los que se les sacará el Gini
      * @param column, columna en dónde se evaluará el método
      * @param conditions, condiciones para evaluar el gini
@@ -173,7 +201,7 @@ public class ImpurezaGini{
         String temp = "";
         double tempMin = 0;
         for(int j = 0; j<conditions.length; j++){
-            y = calculateGiniImpurity((column, r, conditions[j]).getGini());
+            y = calculateGiniImpurity(column, r, conditions[j]).getGini();
             temp = String.valueOf(y);
             tempMin = Double.parseDouble(temp);
             if(tempMin < min){
