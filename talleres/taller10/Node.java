53db463125cc22694e898e857df270e39ad3ec19
Valentina Moreno Ramírez <68928379+vmorenor16@users.noreply.github.com>
2020-10-16 14:59:51 -0500

    Add files via upload

---
+++ talleres/taller10/Node.java
@@ -0,0 +1,11 @@
+
+public class Node {
+    public Node left;
+    public Node right;
+    public int data;
+    public Node(int d){
+       left = null;
+       right = null;
+       data = d;
+    }
+   }
