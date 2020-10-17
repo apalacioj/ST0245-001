b90395a5605c3468192e9e7a305c27f3e731b156
apalacioj <68928378+apalacioj@users.noreply.github.com>
2020-10-16 22:46:09 -0500

    Cambios en el taller 10

--- talleres/taller10/BinaryTree.java
+++ talleres/taller10/BinaryTree.java
@@ -1,22 +1,21 @@
+
 /**
- * La clase BinaryTree intenta dar un conocimiento a los
- * estudiantes acerca del manejo de un arbol binario de enteros.  
- * Aquí se espera crear una estrcutura concreta del árbol binario,
- * cabe aclarar que esta estrucutra ya esta implementada.
- * @author Mauricio Toro, Andres Paez, Valentina Moreno y Alejandra Palacio
- * @version 16/10/2020
- * 
+ * Esta clase contiene la solución al taller 10 sobre árboles binarios de búsqueda.
+ * @author Valentina Moreno Ramírez y Alejandra Palacio Jaramillo
+ * @version 2
  */
-
-public class BinaryTree{
-
-    // Sirve como raíz del árbol
+public class BinaryTree
+{
     public Node root;
 
+    public BinaryTree(Node root){
+        this.root = root;
+    }
+
     /**
-     * @param n el dato del nodo que se busca buscar
-     * Metodo auxiliar de insetarAux.
-     *
+     * Este método inserta un dato en un árbol binario de búsqueda.
+     * @param n el dato del nodo que se quiere insertar.
+     * @see método insertarAux
      */
     public void insertar(int n) {
         if (root == null) {
@@ -27,134 +26,103 @@ public class BinaryTree{
     }
 
     /**
-     * @param node es la raíz del arbol
-     * @param n el data del nodo que se busca insertar
-     * nota: metodo recursivo.
+     * Este método inserta un dato en el árbol binario de búsqueda de acuerdo con las desigualdades.
+     * Agrega a la izquierda del nodo los valores menores que el que contiene el nodo y a la derecha, los mayores o iguales.
+     * @param node es la raíz del arbol.
+     * @param n el dato del nodo que se quiere insertar.
      * Inserta un dato respetando claro las desigualdades en el árbol
      */
-    private void insertarAux(Node node, int n, Node ant) { //Complejidad O(log(n))
-        //...
+    private void insertarAux(Node node, int n, Node anterior) { 
         if (node == null) {
-            if (n >= ant.data) {
-                ant.right = new Node(n); 
+            if (n >= anterior.data) {
+                anterior.right = new Node(n); 
             } else {
-                ant.left = new Node(n);
+                anterior.left = new Node(n);
             }
+        }else if (n < node.data){
+            insertarAux(node.left , n, node);
+        }else{
+            insertarAux(node.right, n, node);      
         }
-        else if (n < node.data) insertarAux(node.left , n, node); //T(n) = T(n/2)
-        else insertarAux(node.right, n, node);                    //T(n) = T(n/2)        
-    }                                                       //T(n) = T(n/2)   O(log(n))   
+        //Complejidad O(log(n))
+    }                                                        
 
     /**
+     * Este método busca si un dato específico se encuentra en el árbol.
      * @param n el dato del nodo que se busca.
-     * Metodo auxiliar de buscarAux.
-     * @return true si el metodo buscarAux es true
+     * @return true si el metodo buscarAux es true, es decir, si se encuentra el elemento.
+     * @see método buscarAux
      */
     public boolean buscar(int n) {
         return buscarAux(root, n);
     }
 
     /**
-     * @param node es la raíz del arbol
-     * @param n el data del nodo que se busca
-     * nota: metodo recursivo.
-     * Inserta un dato respetando claro las desigualdades en el árbol
-     * @return true si se encuentra el elemento en el árbol, false de lo contrario
+     * Este método busca en un árbol si cierto elemento se encuentra en él.
+     * @param node es la raíz del arbol.
+     * @param n el data del nodo que se busca.
+     * @return true si se encuentra el elemento en el árbol, de lo contrario, false.
      */ 
 
     private boolean buscarAux(Node node, int n) { //Complejidad O(log(n))
-        if (node == null) 
+        if (node == null){ 
             return false;
-        //Si estoy aquí es porque no retornó falso, y si es así
-        //es porque el nodo no es null
-        if (node.data == n)
-            return true; //Pero si es igual, no sería que encontramos el nodo que buscabamos?
-        if (n < node.data) {
-            //boolean estaALaIzquierda = buscarAux(node.left, n);
-            return buscarAux(node.left, n);//Está a la izquierda?
         }
-        return buscarAux(node.right, n); // T(n) = T(n/2) + c es O(log n)
+        if (node.data == n){
+            return true; 
+        }
+        if (n > node.data) {
+            return buscarAux(node.right, n);
+        }
+        return buscarAux(node.left, n); 
     }
 
-    public void visualizar() {
-        System.out.print("Inserta el siguiente codigo en la pagina: ");
+    /**
+     * Este método imprime el resultado del método codigoAux, lo cual se debe insertar en la página www.webgraphviz.com para ver el árbol dibujado.
+     * @see método codigoAux
+     */
+    public void codigo() {
+        System.out.println("Inserta el siguiente codigo en la pagina: ");
         System.out.println("http://www.webgraphviz.com/");
-        visualizarAux(root);
+        codigoAux(root);
     }
 
-    private void visualizarAux(Node n) {  //Complejidad O(n) || con n la cantidad de nodos
-        if (n.left == null){ 
-            return;
-        }else{
+    /**
+     * Este método imprime en determinado formato la manera en que están organizados los nodos en el árbol, de acuerdo a las desigualdades del mismo.
+     * @param n es la raíz del árbol.
+     */
+    private void codigoAux(Node n) {
+        boolean a = false;
+        boolean b = false;
+        if(n.left != null && n.right != null){
             System.out.println("\"" + n.data + "\"" + " -> " + "\"" + n.left.data + "\"");
-        }
-        if (n.right == null){
-            return;
-        }else{
+           codigoAux(n.left);
             System.out.println("\"" + n.data + "\"" + " -> " + "\"" + n.right.data + "\"");
+            codigoAux(n.right);
+        }else if(n.left == null && n.right !=null){
+            System.out.println("\"" + n.data + "\"" + " -> " + "\"" + n.right.data + "\"");
+            codigoAux(n.right);
+        }else if(n.left != null && n.right == null){
+            System.out.println("\"" + n.data + "\"" + " -> " + "\"" + n.left.data + "\"");
+            codigoAux(n.left);
         }
-        visualizarAux(n.left);  //T(n) = T(n/2) + c
-        visualizarAux(n.right); //T(n) = T(n/2) + c
-    }                           //T(n) = 2T(n/2) + c
+        //T(n) = 2T(n/2) + c
+        //Complejidad = O(n)
+    }                           
 
-    // /**
-    // * @param n el dato del nodo que se busca borrar.
-    // * Metodo auxiliar de buscarAux.
-    // * 
-    // */
-    // public void borrar(int n) {
-    // borrarAux(root, n);
-    // }
-
-    // /**
-    // * @param node es la raíz del arbol
-    // * @param n el data del nodo que se busca borrar
-    // * nota: metodo recursivo.
-    // * borra un dato respetando claro las desigualdades en el árbol
-    // */ 
-    // private Node borrarAux(Node node, int n) {
-    // //Node ant = nodoAnterior(node, n);
-    // //Node nuevo = nodo
-    // //return elMayorDeLosMenores(node);
-    // }
-
-    private int elMayorDeLosMenores(Node node){
-        return elMayorYNoEsNull(node.left);
-    }
-
-    private int elMayorYNoEsNull(Node node) {
-        if (node.left == null && node.right == null)
-            return node.data;
-        else
-            return elMayorYNoEsNull(node.right);
-    }
-
-    /*
-    Los metodos posteriores son para imprimir el arbol de manera
-    que sirven para debuggeo
-     */
-
-    /**
-     * @param node el nodo desde el cual se imprimirá
-     * imprime el arbol desde el nodo node
-     */
-    private void recursivePrintAUX(Node node)
-    {
-        if (node != null)
-        {
-            recursivePrintAUX(node.left);
-            System.out.println(node.data);
-            recursivePrintAUX(node.right);            
-        }
-
-    }
-
-    /**
-     * Metodo auxiliar de recursivePrintAUX
-     *
-     */
-    public void recursivePrint()
-    {
-        recursivePrintAUX(root);
+    public static void main(String [] args){
+        BinaryTree Tree = new BinaryTree(new Node(4));
+        Tree.insertar(3);
+        Tree.insertar(7);
+        Tree.insertar(2);
+        Tree.insertar(6);
+        Tree.insertar(3);
+        Tree.insertar(1);
+        Tree.insertar(9);
+        Tree.insertar(10);
+        Tree.codigo();
+        System.out.println(Tree.buscar(7));
+        System.out.println(Tree.buscar(10));
+        System.out.println(Tree.buscar(20));
     }
 }
